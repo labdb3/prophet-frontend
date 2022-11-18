760149b21712d3f30f8c0e6a46a6c5969a72ccdc
zdliu2021 <1475879051@qq.com>
2022-11-18 14:39:07 +0800

    WIP

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios' // 引入axios
 
 const service = axios.create({
-  // baseURL: "http://162.105.88.120:8000",
+  // baseURL: "http://162.105.88.116:8000",
   baseURL: "http://localhost:8000",
   timeout: 1000*60*24
 })
--- src/views/ModelView.vue
+++ src/views/ModelView.vue
@@ -2,19 +2,29 @@
   <p></p>
   <main>
     选择保存的prophet模型:&nbsp;&nbsp;
-    <selectVue :options="all_models_1" @getSelectedDataset="selectModels_1"></selectVue>
+    <el-select v-model="select_model_1" class="m-2" placeholder="Select" size="large">
+        <el-option v-for="item in all_models_1" :key="item.value" :label="item.label" :value="item.value" />
+    </el-select>
   </main>
   <main>
-  选择保存的翁氏模型:&nbsp;&nbsp;
-    <selectVue :options="all_models_2" @getSelectedDataset="selectModels_2"></selectVue>
+    选择保存的翁氏模型:&nbsp;&nbsp;
+    <el-select v-model="select_model_2" class="m-2" placeholder="Select" size="large">
+        <el-option v-for="item in all_models_2" :key="item.value" :label="item.label" :value="item.value" />
+    </el-select>
   </main>
   <main>
-  选择保存的灰度预测模型:&nbsp;&nbsp;
-    <selectVue :options="all_models_3" @getSelectedDataset="selectModels_3"></selectVue>
+    选择保存的灰度预测模型:&nbsp;&nbsp;
+    <el-select v-model="select_model_3" class="m-2" placeholder="Select" size="large">
+        <el-option v-for="item in all_models_3" :key="item.value" :label="item.label" :value="item.value" />
+    </el-select>
+  </main>
+  <main>
+    选择数据集:&nbsp;&nbsp;
+    <SelectVue :options="all_datasets" @getSelectedDataset="selectDataset"> </SelectVue>
   </main>
   <p></p>
   <p>
-  预测年数: &nbsp;&nbsp;
+    预测年数: &nbsp;&nbsp;
     <el-input
       v-model="years"
       style="width: 180px"
@@ -31,69 +41,76 @@
 </template>
 
 <script>
-import Upload from '../components/upload.vue'
-import SelectVue from '../components/select.vue';
-import service from '../utils/request';
-import EchartsLoadModel_mutli from "../components/EchartsLoadModel_mutli.vue"
+import Upload from "../components/upload.vue";
+import SelectVue from "../components/select.vue";
+import service from "../utils/request";
+import EchartsLoadModel_mutli from "../components/EchartsLoadModel_mutli.vue";
 
 export default {
   name: "dataset",
   data() {
     return {
-      select_model_1: '',
-      select_model_2: '',
-      select_model_3: '',
+      all_datasets: [],
+      selected_dataset: "",
+      select_model_1: "",
+      select_model_2: "",
+      select_model_3: "",
       all_models_1: [],
       all_models_2: [],
-      all_models_3:[],
+      all_models_3: [],
       echarts_models: {},
-      years:5,
-    }
+      years: 5,
+    };
   },
   props: {},
   methods: {
-    selectModels_1(models) {
-      this.select_model_1 = models
-    },
-    selectModels_2(models) {
-      this.select_model_2 = models
-    },
-    selectModels_3(models) {
-      this.select_model_3 = models
+    selectDataset(dataset) {
+      this.selected_dataset = dataset;
+      this.select_model_1 = '';
+      this.select_model_2 = '';
+      this.select_model_3 = '';
+      service.get("getSavedModels?model=灰度预测&dataset="+this.selected_dataset).then((response) => {
+        console.log(response.data);
+        this.all_models_3 = response.data;
+      });
+      service.get("getSavedModels?model=prophet&dataset="+this.selected_dataset).then((response) => {
+        console.log(response.data);
+        this.all_models_1 = response.data;
+      });
+      service.get("getSavedModels?model=翁氏模型&dataset="+this.selected_dataset).then((response) => {
+        console.log(response.data);
+        this.all_models_2 = response.data;
+      });
     },
     fitting() {
-      console.log("prophet:",this.select_model_1,this.select_model_2,this.select_model_3)
-      service.post("loadModel_multi", {
-        "models": {
-          "prophet": [this.select_model_1],
-          "灰度预测": [this.select_model_3],
-          "翁氏模型":[this.select_model_2],
-        },
-        "years":parseInt(this.years),
-      }).then(
-        (response) => {
-          this.echarts_models = response.data
-        }
-      )
-    }
+      console.log(
+        "prophet:",
+        this.select_model_1,
+        this.select_model_2,
+        this.select_model_3
+      );
+      service
+        .post("loadModel_multi", {
+          models: {
+            prophet: [this.select_model_1],
+            灰度预测: [this.select_model_3],
+            翁氏模型: [this.select_model_2],
+          },
+          years: parseInt(this.years),
+        })
+        .then((response) => {
+          this.echarts_models = response.data;
+        });
+    },
   },
   mounted() {
-    service.get("getSavedModels?model=灰度预测").then((response) => {
-      console.log(response.data);
-      this.all_models_3 = response.data;
-    });
-    service.get("getSavedModels?model=prophet").then((response) => {
-      console.log(response.data);
-      this.all_models_1 = response.data;
-    });
-    service.get("getSavedModels?model=翁氏模型").then((response) => {
-      console.log(response.data);
-      this.all_models_2 = response.data;
+    service.get("getAllDatasets").then((response) => {
+      this.all_datasets = response.data;
+      console.log("all datasets:", this.all_datasets);
     });
   },
-  components: { Upload, SelectVue,  EchartsLoadModel_mutli }
+  components: { Upload, SelectVue, EchartsLoadModel_mutli },
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
