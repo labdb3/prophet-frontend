a38c0dfd8252ab0513ee92f310a3dc97cd02ed5f
liuzongdian <1475879051@qq.com>
2023-03-12 13:11:01 +0800

    fix

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios' // 引入axios
 
 const service = axios.create({
   // baseURL: "http://162.105.88.116:8000",
-  baseURL: "http://localhost:8000",
+  baseURL: "http://127.0.0.1:8000",
   timeout: 1000*60*24
 })
 
